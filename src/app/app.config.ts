c2db3e86d4e3d8797c4e08088bd6f5cb9d1fe0ef
achreftun <elmouelhi.achref@gmail.com>
2025-11-26 17:02:11 +0100

    updating App component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+// la configuration globale d'une application Angular
+
 export const appConfig: ApplicationConfig = {
+  // fournisseurs
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes)
