8ca5bbb75219cb14fe20fe618f20630d15a88f2b
achreftun <elmouelhi.achref@gmail.com>
2025-11-28 09:26:32 +0100

    RxJS + Routage + Binding

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { ObservableComponent } from './components/observable/observable';
+import { SubjectComponent } from './components/subject/subject';
+import { ComputedComponent } from './components/computed/computed';
+import { HomeComponent } from './components/home/home';
+import { AboutComponent } from './components/about/about';
+import { AdresseComponent } from './components/adresse/adresse';
+import { StagiaireComponent } from './components/stagiaire/stagiaire';
+import { CalculComponent } from './components/calcul/calcul';
+import { TableauComponent } from './components/tableau/tableau';
+
 // ici on définit les routes de nos composants
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'tableau/:indice', component: TableauComponent },
+    { path: 'adresse', component: AdresseComponent },
+    { path: 'calcul/:op', component: CalculComponent },
+    { path: 'stagiaire/:nom/:prenom', component: StagiaireComponent },
+    { path: 'observable', component: ObservableComponent },
+    { path: 'subject', component: SubjectComponent },
+    { path: 'computed', component: ComputedComponent },
+];
---
+++ src/app/components/about/about.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import Stagiaire from '../../classes/stagiaire';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-about',
+  imports: [CommonModule, RouterLink],
+  templateUrl: './about.html',
+  styleUrl: './about.css',
+})
+export class AboutComponent {
+  //  attributs
+  isDisabled = true
+  title = 'angular-standalone';
+  stagiaire = new Stagiaire(100, "Wick", "John")
+  stagiaire2 = new Stagiaire(100, "Dalton")
+  numbers = [2, 3, 8, 5, 1]
+  stagiaires: Array<Stagiaire> = [
+    new Stagiaire(100, 'Wick', 'John'),
+    new Stagiaire(101, 'Abruzzi', 'John'),
+    new Stagiaire(102, 'Marley', 'Bob'),
+    new Stagiaire(103, 'Segal', 'Steven')
+  ];
+  couleur = 'white'
+  bgCouleur = 'tomato'
+  style = {
+    color: 'white',
+    backgroundColor: 'dodgerblue'
+  }
+  moyennes: number[] = [18, 5, 11, 15]
+  firstname = "Kostas"
+  lastname = "Mitroglou"
+  ville = "Toulouse"
+  codePostal = "31000"
+  // méthodes
+  afficherBonjour() {
+    return "Bonjour"
+  }
+  alertBonjour() {
+    alert("Bonjour")
+  }
+  switchState() {
+    this.isDisabled = !this.isDisabled
+  }
+  afficherTexte(event: Event) {
+    // le dernier caractère saisi
+    console.log((event as InputEvent).data);
+
+    // le contenu de l'élément HTML input
+    console.log((event.target as HTMLInputElement).value);
+
+  }
+}
