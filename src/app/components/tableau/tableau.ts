c348a0a3451c52e86bde2b9af5a02add545b07eb
achreftun <elmouelhi.achref@gmail.com>
2025-11-28 10:09:47 +0100

    using Router service in TableauComponent

--- src/app/components/tableau/tableau.ts
+++ src/app/components/tableau/tableau.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tableau',
@@ -12,7 +12,10 @@ export class TableauComponent implements OnInit {
   numbers = [2, 3, 8, 5, 1]
   next = 0
   previous = 0
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
   ngOnInit(): void {
     this.route.paramMap.subscribe(p => {
       this.indice = Number(p.get('indice'))
@@ -20,6 +23,12 @@ export class TableauComponent implements OnInit {
       this.next = (this.indice + 1) % this.numbers.length
       this.previous = (this.indice) == 0 ? this.numbers.length - 1 : this.indice - 1
     })
+  }
+  goToPrevious() {
+    this.router.navigate(['/tableau', this.previous])
+  }
+  goToNext() {
+    this.router.navigate(['/tableau', this.next])
   }
 }
 
