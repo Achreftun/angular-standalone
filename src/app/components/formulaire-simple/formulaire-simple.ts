c334ec5354a45ee2a60f2b8c60d35c02bffef600
achreftun <elmouelhi.achref@gmail.com>
2025-11-28 12:02:44 +0100

    Two way binding with ngModel (FormsModule)

--- src/app/components/formulaire-simple/formulaire-simple.ts
+++ src/app/components/formulaire-simple/formulaire-simple.ts
@@ -1,17 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulaire-simple',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './formulaire-simple.html',
   styleUrl: './formulaire-simple.css',
 })
 export class FormulaireSimpleComponent {
   nom = 'Doe'
-  updateNom(value: string) {
-    this.nom = value
-  }
-  // updateNom(event: Event) {
-  //   this.nom = (event.target as HTMLInputElement).value
-  // }
+
 }
