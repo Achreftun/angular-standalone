2e3eacea654630266bd7fc912fdb049ba5da2ec7
achreftun <elmouelhi.achref@gmail.com>
2025-12-05 11:34:14 +0100

    Defining a counter store

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from "./components/shared-module/menu/menu";
-import {  HeaderComponent } from "./components/shared-module/header/header";
+import { HeaderComponent } from "./components/shared-module/header/header";
 
 
 @Component({
   selector: 'app-root', // la balise à utiliser pour ce composant
-  imports: [ RouterOutlet, MenuComponent, HeaderComponent], // CommonModule = pipes et directives de base
+  imports: [RouterOutlet, MenuComponent, HeaderComponent], // CommonModule = pipes et directives de base
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
